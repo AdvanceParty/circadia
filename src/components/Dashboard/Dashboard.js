e0017a11e7af5a5f19301201c2689adbad6033ee
Gil Fewster <hello@gilfewster.com>
2020-03-30 23:16:46 +1100

    minor tweaks to dashboard data model to align with updated API

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,13 +3,13 @@ import useSlackApi from '../../useSlackApi';
 import styles from './Dashboard.module.scss';
 
 const IMAGE_SIZES = {
-  SMALL: 's',
-  MEDIUM: 'm',
-  LARGE: 'l'
+  SMALL: 48,
+  MEDIUM: 192,
+  LARGE: 512
 };
 
 function Dashboard() {
-  const [imageSize, setImageSize] = useState(IMAGE_SIZES.MEDIUM);
+  const [imageSize, setImageSize] = useState(IMAGE_SIZES.SMALL);
   const { isLoading, error, data, status } = useSlackApi('/users/list');
   const forbidden = status === 403;
 
@@ -22,15 +22,34 @@ function Dashboard() {
   const userStatus = userData => {
     const {
       id,
-      item: { displayName, statusText, images }
+      item: {
+        profile: { displayName, realName, title, status, images }
+      }
     } = userData;
-    const image = images[imageSize];
+
+    const image =
+      images.filter(image => image.size == imageSize)[0] || images[0];
+
+    const useName = displayName || realName || 'Secret Squirrel';
+
     return (
       <div className={styles.userStatus} key={id}>
-        <img src={image} alt={displayName} />
+        <img
+          src={image.src}
+          width={image.size}
+          height={image.size}
+          alt={useName}
+        />
         <div>
-          <h5 className={styles.title}>{displayName}</h5>
-          <p>{statusText}</p>
+          <h5 className={styles.title}>{useName}</h5>
+          {title ? (
+            <p>
+              <em>{title}</em>
+            </p>
+          ) : (
+            ''
+          )}
+          <p>{status.text}</p>
         </div>
       </div>
     );
